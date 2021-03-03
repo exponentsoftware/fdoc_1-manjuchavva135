c30f49345799cc60ac5c4eb12e1d0398d94bc9fd
manjuchavva135 <52740830+manjuchavva135@users.noreply.github.com>
2021-03-03 11:49:26 +0530

    written logic to count words

--- one/a.js
+++ one/a.js
@@ -1,1 +1,8 @@
 function countWords(string, word) {
+    return string.split(word).length - 1;
+ }
+ var text="I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love."; 
+ var count=countWords(text,"love");
+ console.log(count)
+ var count2=countWords(text,"you");
+ console.log(count2)
