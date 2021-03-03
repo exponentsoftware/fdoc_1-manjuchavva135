3d13060971d098316c39e296cef5a2783c471259
manjuchavva135 <52740830+manjuchavva135@users.noreply.github.com>
2021-03-03 12:00:10 +0530

    replacing all the strings that does not match a-z A-Z  . and searching globally.

---
+++ one/b.js
@@ -0,0 +1,3 @@
+var str = "%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?";
+d=str.replace(/[^a-zA-Z .]/g, "");
+console.log(d)
